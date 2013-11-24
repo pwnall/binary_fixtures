319b769afb9d8108f8a5347c42ce0b7659fc97af
Victor Costan <costan@gmail.com>
2013-11-24 14:16:10 -0500

    Revised Rails patch 13022.

--- lib/binary_fixtures/rails_13022.rb
+++ lib/binary_fixtures/rails_13022.rb
@@ -3,28 +3,24 @@ require 'active_record/fixtures'
 
 # Packaged version of https://github.com/rails/rails/pull/13022
 unless defined?(ActiveRecord::FixtureSet::RenderContext)
-  module ActiveRecord
-    class FixtureSet
-      # The context used by the ERB fixture renderer.
-      #
-      # Make helper functions available in your fixtures by defining them in a
-      # module and including it in ActiveRecord::FixtureSet::RenderContext.
-      #
-      # A new subclass of this class is created each time the ERB renderer is
-      # called so that methods defined in ERB templates do not leak into other
-      # templates' context.
-      class RenderContext
-        # An instance of a new RenderContext subclass.
-        def self.create
-          klass = ::Class.new(ActiveRecord::FixtureSet::RenderContext) do
-            def get_binding
-              binding()
-            end
-          end
-
-          klass.new
+  # The context used by the ERB fixture renderer.
+  #
+  # Make helper functions available in your fixtures by defining them in a
+  # module and including it in ActiveRecord::FixtureSet::RenderContext.
+  #
+  # A new subclass of this class is created each time the ERB renderer is
+  # called so that methods defined in ERB templates do not leak into other
+  # templates' context.
+  class ActiveRecord::FixtureSet::RenderContext
+    # An instance of a new RenderContext subclass.
+    def self.create
+      klass = ::Class.new(ActiveRecord::FixtureSet::RenderContext) do
+        def get_binding
+          binding()
         end
       end
+
+      klass.new
     end
   end
 
